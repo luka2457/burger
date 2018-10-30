f7fd1579d1fcbe4e0e02456a6a26f143620e17b8
Luke Karlovich <luke.karlovich@colorado.edu>
2018-10-30 14:10:01 -0600

    before heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO cats (name) VALUES ('Cheese');
+INSERT INTO cats (name) VALUES ('Bacon');
+INSERT INTO cats (name, sleepy) VALUES ('Spicy', true);
+INSERT INTO cats (name, sleepy) VALUES ('Lots of pickles', true);
+INSERT INTO cats (name, sleepy) VALUES ('Everything burger', true);
